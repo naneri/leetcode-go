0fbc5e081d4121a8cf224373c5954974f7ce958e
Kana <ktnaneri@gmail.com>
2023-08-25 11:15:52 +0600

    Update rearrange-spaces-between-words.go

--- rearrange-spaces-between-words.go
+++ rearrange-spaces-between-words.go
@@ -3,21 +3,12 @@
 func reorderSpaces(text string) string {
 
 	var spaceCount int
-	var wordCount  int
-	var newWord []rune
 	words := strings.Fields(text)
 	
 	for _, char := range text {
 		if char == ' ' {
 			spaceCount++
 		}
-	}
-
-	if len(newWord) > 0 {
-		anotherWord := string(newWord)
-		words = append(words, anotherWord)
-		newWord = make([]rune, 0)
-		wordCount++
 	}
 
 	if len(words) == 1 {
